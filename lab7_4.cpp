a90e2e5ecdd9623cdf139bffff59e670ee45e852
xchalarm <tharana4671@gmail.com>
2022-12-13 16:37:33 +0700

    lab7_4.cpp completed

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 	
-	string name[10] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
+	string name[10] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"} ;
 	int id[10] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[10] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
     
